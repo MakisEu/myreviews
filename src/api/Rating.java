335f89a7af0fe8ea0e5d08b781a04ebd3c431ec1
Dimitris <dntantak@csd.auth.com>
2022-11-17 18:41:25 +0200

    1st commit

---
+++ src/api/Rating.java
@@ -0,0 +1,33 @@
+package api;
+
+public class Rating {
+
+    private String rating_description,name_of_user;
+    private float grade;
+
+    public Rating(String description,float grade)//,String name_of_user)
+    {
+        rating_description=description;
+        this.grade=grade;
+        //this.name_of_user=name_of_user;
+    }
+
+    public String getRatingDescription()
+    {
+        return rating_description;
+    }
+
+    public float getGrade()
+    {
+        return grade;
+    }
+
+    public void setRatingDescription(String rating_description){
+        this.rating_description=rating_description;
+    }
+
+    public void setGrade(float grade){
+        this.grade=grade;
+    }
+
+}
