d9775a0d7671cb536df4795679e5124aa1cdf060
Dimitris <dntantak@csd.auth.com>
2022-11-18 19:27:49 +0200

    2nd commit

---
+++ src/api/Properties.java
@@ -0,0 +1,81 @@
+package api;
+
+public class Properties {
+    private String view,bath,laundry,entertainment,heating,internet,kitchen_room,outside,parking;
+
+    public String getView(){
+        return view;
+    }
+
+    public void setView(){
+        this.view=view;
+    }
+
+    public String getBath(){
+        return bath;
+    }
+
+    public String getLaundry(){
+        return laundry;
+    }
+
+    public String getEntertainment(){
+        return entertainment;
+    }
+
+    public String getHeating(){
+        return heating;
+    }
+
+    public String getInternet(){
+        return internet;
+    }
+
+    public String getKitchen_room(){
+        return kitchen_room;
+    }
+
+    public String getOutside(){
+        return outside;
+    }
+
+    public String getParking(){
+        return parking;
+    }
+
+    public void setBath(String bath){
+        this.bath=bath;
+    }
+
+    public void setLaundry(String laundry){
+        this.laundry=laundry;
+    }
+
+    public void setView(String view){
+        this.view=view;
+    }
+
+    public void setHeating(String heating){
+        this.heating=heating;
+    }
+
+    public void setInternet(String internet){
+        this.internet=internet;
+    }
+
+    public void setKitchen_room(String kitchen_room){
+        this.kitchen_room=kitchen_room;
+    }
+
+    public void setOutside(String outside){
+        this.outside=outside;
+    }
+
+    public void setParking(String parking){
+        this.parking=parking;
+    }
+
+    public void setEntertainment(String entertainment) {
+        this.entertainment = entertainment;
+    }
+}
--- src/api/Rating.java
+++ src/api/Rating.java
@@ -2,7 +2,7 @@ package api;
 
 public class Rating {
 
-    private String rating_description,name_of_user;
+    private String rating_description,name_of_user,date;
     private float grade;
 
     public Rating(String description,float grade,String name_of_user)
@@ -11,6 +11,16 @@ public class Rating {
         this.grade=grade;
         this.name_of_user=name_of_user;
     }
+
+    public Rating(String description,float grade,String name_of_user,String date)
+    {
+        rating_description=description;
+        this.grade=grade;
+        this.name_of_user=name_of_user;
+        this.date=date;
+    }
+
+
 
     public String getRatingDescription()
     {
@@ -35,6 +45,12 @@ public class Rating {
 
     }
 
+    public void setDate(String date){
+        this.date=date;
+    }
+    public String getDate(){
+        return date;
+    }
 
 
 }
