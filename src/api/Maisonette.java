add18fbfd92125a4d8177b602fc0f3cbe201a63a
Ioakeime Euaggelinos <euaggelinosmakis@gmail.com>
2022-11-19 18:50:43 +0200

    Added Maisonette and test.

--- src/api/Maisonette.java
+++ src/api/Maisonette.java
@@ -1,5 +1,29 @@
 package api;
 
 public class Maisonette extends  Accommodation{
+    private int roommates=0;
+    public  Maisonette(){super();}
+    public  Maisonette(String n,String a, String tk,String c,String desc,String o,int rm){
+        super(n,a,tk,c,desc,o);
+        roommates=rm;
+    }
+    public  Maisonette(String n,String a, String tk,String c,String desc,String o){
+        super(n,a,tk,c,desc,o);
+    }
 
+    public int getRoommates() {
+        return roommates;
+    }
+
+    public void setRoommates(int roommates) {
+        this.roommates = roommates;
+    }
+
+    @Override
+    public void show() {
+        System.out.println("Accommodation Type: Maisonette");
+        super.showNR();
+        System.out.println("Roommates:      "+String.valueOf(roommates));
+        super.showRatings();
+    }
 }
