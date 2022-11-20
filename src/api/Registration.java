f1a67576fe3f9cc02beba5831e315362c3caa900
Dimitris <dntantak@csd.auth.com>
2022-11-20 17:25:07 +0200

    added 2 new classes , registered and registration

---
+++ src/api/Registration.java
@@ -0,0 +1,46 @@
+package api;
+
+import java.util.ArrayList;
+
+public class Registration {
+
+    private String Username,Password,Name,Surname,user_type;
+
+    public Registration(String username,String password,String name,String surname,String type){
+        Username=username;
+        Password=password;
+        Name=name;
+        Surname=surname;
+        user_type=type;
+    }
+
+    public String getUserType(){
+        return user_type;
+    }
+
+    public String getUsername(){
+        return this.Username;
+    }
+
+    public void setUsername(String username){
+        Username=username;
+    }
+
+
+  /*  public boolean check_username(String username,String type,Registered r){
+        if (r.username_availability(username,type))
+            {
+            r.add_registration(this);
+            return true;
+            }
+        else
+            return false;
+    }*/
+
+
+}
+
+
+
+
+
